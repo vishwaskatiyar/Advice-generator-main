f41b6670aae61ae1862a829c44789a78bd9b0c17
Vishwas katiyar <vishwaskatiyar39@gmail.com>
2024-07-23 16:53:36 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
+import diceimage from "./Assit/icon-dice.svg";
+import divider from "./Assit/pattern-divider-desktop.svg";
 
 const App = () => {
   const [advice, setAdvice] = useState("");
@@ -39,17 +41,13 @@ const App = () => {
           <p className="text-2xl mb-6 font-bold">{advice}</p>
         )}
         <div className="flex justify-center mb-6">
-          <img
-            src="../../Assit/pattern-divider-desktop.svg"
-            alt="divider"
-            className="w-full"
-          />
+          <img src={divider} alt="divider" className="w-full" />
         </div>
         <button
           onClick={fetchAdvice}
           className="bg-neon-green p-4 rounded-full hover:shadow-lg transition absolute -bottom-6 left-1/2 transform -translate-x-1/2"
         >
-          <img src="../../Assit/icon-dice.svg" alt="dice" />
+          <img src={diceimage} alt="dice" />
         </button>
       </div>
     </div>
